511e4930ab1c0dbe0cfd8f93b70847296a6726a4
hanjiatong <603421579@qq.com>
2024-11-20 21:35:42 +0800

    fc

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
 //请在下方输入你的代码：
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=9;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d*%d=%2d ",i,j,i*j);
+        }
+        printf("\n");
+    }
+    int i,j;
+    for(i=9;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d*%d=%2d ",i,j,i*j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
